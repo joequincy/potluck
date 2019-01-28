2186a4613d6bd4247026cf5504a7c7690d363b41
Jon Peterson <jon@pnwebs.com>
2019-01-28 11:48:03 -0700

    Add tests for Dish

---
+++ test/dish_test.rb
@@ -0,0 +1,24 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/dish'
+
+class DishTest < Minitest::Test
+  def setup
+    @dishes = [
+      Dish.new("Couscous Salad", :appetizer),
+      Dish.new("Chips and Dip", :appetizer),
+      Dish.new("Lasagne",:entree)
+    ]
+  end
+  def test_dish_exists
+    assert_instance_of Dish, @dishes[0]
+  end
+  def test_dish_has_correct_attributes
+    assert_equal "Couscous Salad", @dishes[0].name
+    assert_equal :appetizer, @dishes[0].category
+    assert_equal "Chips and Dip", @dishes[1].name
+    assert_equal :appetizer, @dishes[1].category
+    assert_equal "Lasagne", @dishes[2].name
+    assert_equal :entree, @dishes[2].category
+  end
+end
